43d6f2f89c7a290a13bf454a71702fc2c0a3588a
Jauhar009 <jauhar009@gmail.com>
2023-09-24 16:25:25 +0530

    Initial commit after adding.

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+//import java.lang.runtime.SwitchBootstraps;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+    }
---
+++ src/loops.java
@@ -0,0 +1,7 @@
+import java.util.*;
+class loops {
+    public static void main(String[] args) {
+       Scanner Sc = new Scanner(System.in);
+
+    }
+}
