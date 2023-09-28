29c9cddc0d55a7103aead111fac8262c50cc0e32
Md-FaisalAli <mdfaisalali444@gmail.com>
2023-09-28 16:49:55 +0530

    Edited Pattern Generator program.

--- src/PatternGenerator.java
+++ src/PatternGenerator.java
@@ -1,8 +1,8 @@
-
+import  java.util.*;
 public class PatternGenerator {
     public static void main(String[] args) {
-
-        int n = 5;
+Scanner sc= new Scanner(System.in);
+        int n = sc.nextInt();
 
         for (int i = 1; i<=n; i++){
             for (int j= 1; j<=i; j++){
