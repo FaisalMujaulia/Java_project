2ee804e579f97a40f185e0b184eb3b3ff27a0349
Md-FaisalAli <mdfaisalali444@gmail.com>
2024-10-31 20:12:20 +0530

    Add basic Java program for data type examples

---
+++ src/datatypee.java
@@ -0,0 +1,18 @@
+public class datatypee {
+    public static void main(String[] args) {
+        // declaring different type of data types
+        int age = 25;
+        double height = 5.8;
+        char initial = 'j';
+        String name = "faisal";
+        boolean isAdult= true;
+
+        // printing variables in the console
+        System.out.println("Name= " + name);
+        System.out.println("Age= " + age);
+        System.out.println("Height= " + height);
+        System.out.println("Initial= "+ initial);
+        System.out.println("Is adult?= "+ isAdult);
+
+    }
+}
